045026f79044ffa897a1eb8bf0ee8d1edf10aed8
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-06-04 23:55:46 +0000

    gofmt

--- database.go
+++ database.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-    "encoding/csv"
-    "fmt"
-    "io"
-    "os"
-    iconv "github.com/djimenez/iconv-go"
+	"encoding/csv"
+	"fmt"
+	iconv "github.com/djimenez/iconv-go"
+	"io"
+	"os"
 )
 
 func ImportDatabase(pfile string) {
 
-    file, err := os.Open(pfile)
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    defer file.Close()
+	file, err := os.Open(pfile)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
 
-    x,_ := iconv.NewReader(file, "iso-8859-1", "utf-8")
-    reader := csv.NewReader(x)
-    reader.Comma = '|'
-    for {
-        record, err := reader.Read()
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            fmt.Println("Error:", err)
-            return
-        }
-        loc := Location{}
-        loc.ImportFromRecord(record)
-        locations = append(locations, loc)
-    }
-    return
+	x, _ := iconv.NewReader(file, "iso-8859-1", "utf-8")
+	reader := csv.NewReader(x)
+	reader.Comma = '|'
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		loc := Location{}
+		loc.ImportFromRecord(record)
+		locations = append(locations, loc)
+	}
+	return
 }
-
