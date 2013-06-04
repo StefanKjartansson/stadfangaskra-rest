24d5d341052c9a4d87e0055928cbcf8bc39f5c46
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-06-04 23:35:45 +0000

    first version of query parsing

--- stadfangaskra.go
+++ stadfangaskra.go
@@ -8,8 +8,10 @@ import (
     "io"
     "log"
     "net/http"
+    "net/url"
     "os"
     "strconv"
+    "strings"
     "time"
     iconv "github.com/djimenez/iconv-go"
     isnet "./isnet"
@@ -39,13 +41,12 @@ type Location struct {
 }
 
 
-var placename_file = flag.String("placename_file", "Stadfangaskra_20130326.dsv", "csv input file")
+var placename_file = flag.String("file", "Stadfangaskra_20130326.dsv", "csv input file")
 var locations []Location
-
 const shortForm = "02.01.2006"
 
 
-func InstantiateFromRow(record []string) (loc Location) {
+func RecordToLocation(record []string) (loc Location) {
 
     length := len(record)
 
@@ -74,31 +75,6 @@ func InstantiateFromRow(record []string) (loc Location) {
     return
 }
 
-
-func GetLocation(w http.ResponseWriter, req *http.Request) {
-    log.Printf("%s %s %s", req.RemoteAddr, req.Method, req.URL.Query())
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    w.Write([]byte("["))
-
-    enc := json.NewEncoder(w)
-    hasWritten := false
-
-    for _, element := range locations {
-        if element.Postnr == 108 {
-            if hasWritten {
-                w.Write([]byte(","))
-            }
-            if err := enc.Encode(&element); err != nil {
-                log.Println(err)
-            }
-            hasWritten = true
-        }
-    }
-    w.Write([]byte("]"))
-    return
-}
-
-
 func ImportDatabase(pfile string) {
 
     file, err := os.Open(pfile)
@@ -119,9 +95,151 @@ func ImportDatabase(pfile string) {
             fmt.Println("Error:", err)
             return
         }
-        locations = append(locations, InstantiateFromRow(record))
+        locations = append(locations, RecordToLocation(record))
+    }
+    return
+}
+
+
+func getQueryValue(v url.Values, param string, query *string) error {
+
+    if qval, ok := v[param]; ok {
+        if *query != "" {
+            return fmt.Errorf("Too many queries %v, %v", qval, query)
+        }
+
+        if len(qval) > 1 {
+            return fmt.Errorf("Only accepts a single query parameter %v", qval)
+        }
+        if strings.HasSuffix(param, "_startswith") {
+            *query = qval[0] + "*"
+        } else if strings.HasSuffix(param, "_endswith") {
+            *query = "*" + qval[0]
+        } else {
+            *query = qval[0]
+        }
     }
 
+    return nil
+}
+
+
+func getQueryParamsAsInt(v url.Values, param string) (values []int64) {
+
+    if value, ok := v[param]; ok {
+        for _, i := range value {
+            v, err := strconv.ParseInt(i, 0, 64)
+            if err == nil {
+                values = append(values, v)
+            }
+        }
+    }
+
+    return
+}
+
+func ParseQueryParams(v url.Values) (postcodes []int64, numbers []int64, query string, err error) {
+
+    postcodes = getQueryParamsAsInt(v, "postcode")
+    numbers = getQueryParamsAsInt(v, "number")
+
+    ptr := &query
+    err = getQueryValue(v, "name_startswith", ptr)
+    if err != nil {
+        return
+    }
+
+    err = getQueryValue(v, "name_endswith", ptr)
+    if err != nil {
+        return
+    }
+
+    err = getQueryValue(v, "name", ptr)
+    if err != nil {
+        return
+    }
+    return
+}
+
+
+func (loc Location) ContainsPostcode (list []int64) bool {
+    for _, b := range list {
+        if b == loc.Postnr {
+            return true
+        }
+    }
+    return false
+}
+
+func (loc Location) ContainsNumbers (list []int64) bool {
+    for _, b := range list {
+        if b == loc.Husnr {
+            return true
+        }
+    }
+    return false
+}
+
+
+func (loc Location) NameMatches (query string) bool {
+
+    if query == "" {
+        return true
+    }
+
+    if strings.HasSuffix(query, "*") {
+        v := query[0:strings.Index(query, "*")]
+        if strings.HasPrefix(loc.Heiti_Nf, v) ||
+           strings.HasPrefix(loc.Heiti_Tgf, v) {
+            return true
+        }
+    } else if strings.HasPrefix(query, "*") {
+        v := query[strings.Index(query, "*") + 1:len(query)]
+        if strings.HasSuffix(loc.Heiti_Nf, v) ||
+           strings.HasSuffix(loc.Heiti_Tgf, v) {
+            return true
+        }
+    } else if loc.Heiti_Nf == query ||
+              loc.Heiti_Tgf == query {
+        return true
+    }
+
+    return false
+}
+
+
+func GetLocation(w http.ResponseWriter, req *http.Request) {
+    log.Printf("%s %s %s", req.RemoteAddr, req.Method, req.URL.Query())
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+    postcodes, numbers, query, err := ParseQueryParams(req.URL.Query())
+
+    if err != nil {
+        log.Println(err)
+        w.Write([]byte("Error"))
+        return
+    }
+
+    w.Write([]byte("["))
+
+    enc := json.NewEncoder(w)
+    hasWritten := false
+
+    for _, element := range locations {
+
+        if element.ContainsPostcode(postcodes) &&
+           element.ContainsNumbers(numbers) &&
+           element.NameMatches(query) {
+            if hasWritten {
+                w.Write([]byte(","))
+            }
+            if err := enc.Encode(&element); err != nil {
+                log.Println(err)
+            }
+            hasWritten = true
+        }
+    }
+    w.Write([]byte("]"))
     return
 }
 
