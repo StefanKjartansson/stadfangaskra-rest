50a3566d244927ba8f25728396e4d008bb4d272e
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-07-03 14:59:08 +0000

    added tests for the http handlers

---
+++ handlers_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestHandlers(t *testing.T) {
+
+	const path = "/locations/?"
+
+	tests := []struct {
+		Desc    string
+		Handler func(http.ResponseWriter, *http.Request)
+		Path    string
+		Params  url.Values
+		Status  int
+	}{{
+		Desc:    "",
+		Handler: LocationSearchHandler,
+		Path:    path,
+		Params: url.Values{
+			"postcode": {"101"},
+		},
+		Status: http.StatusOK,
+	}, {
+		Desc:    "",
+		Handler: LocationSearchHandler,
+		Path:    path,
+		Params: url.Values{
+			"postcode": {"101"},
+			"name":     {"Seljavegur"},
+			"number":   {"1"},
+		},
+		Status: http.StatusOK,
+	}, {
+		Desc:    "Single entity",
+		Handler: LocationDetailHandler,
+		Path:    "/locations/10015125/",
+		Status:  http.StatusOK,
+	}}
+
+	for _, test := range tests {
+		record := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", test.Path+test.Params.Encode(), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		test.Handler(record, req)
+		if got, want := record.Code, test.Status; got != want {
+			t.Errorf("%s: response code = %d, want %d", test.Desc, got, want)
+		}
+	}
+}
