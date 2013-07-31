6c8763022f5b183f4cceb7de4f6a1b62ba7f1337
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-07-31 21:56:04 +0000

    moved binary to subfolder

---
+++ bin/stadfangaskra.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"code.google.com/p/gorilla/mux"
+	"flag"
+	"github.com/StefanKjartansson/stadfangaskra"
+	"log"
+	"net/http"
+)
+
+var (
+	router = new(mux.Router)
+)
+
+func main() {
+	var http_listen, placename_file string
+	flag.StringVar(&http_listen, "http", "127.0.0.1:3999", "host:port to listen on")
+	flag.StringVar(&placename_file, "file", "Stadfangaskra_20130326.dsv", "csv input file")
+	flag.Parse()
+
+	log.Println("Starting import")
+	stadfangaskra.ImportDatabase(placename_file)
+	log.Println("Data Imported")
+	log.Println("Starting server")
+
+	stadfangaskra.SetupRoutes(router)
+
+	http.Handle("/", router)
+
+	log.Fatal(http.ListenAndServe(http_listen, nil))
+	log.Println("Bye")
+}
