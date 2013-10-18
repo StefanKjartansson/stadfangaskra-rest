2cc36c402f7e2e6f082bfde155ad46b225d8bdf9
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-10-18 23:43:47 +0000

    equality test for hash function

--- filter_test.go
+++ filter_test.go
@@ -24,3 +24,20 @@ func TestFilter(t *testing.T) {
 	}
 
 }
+
+func TestFilterHash(t *testing.T) {
+
+	f := Filter{
+		Street:   []string{"*vegur"},
+		Postcode: []int{101, 200},
+	}
+	f2 := Filter{
+		Street:   []string{"*vegur"},
+		Postcode: []int{101, 200},
+	}
+
+	if f.Hash() != f2.Hash() {
+		t.Fatalf("f1: %+v should be equal to f2: %+v", f, f2)
+	}
+
+}
