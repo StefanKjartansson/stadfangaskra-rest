92067aa67f7b73d2ad5be371866ecacf77135356
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-10-18 15:41:44 +0000

    refactored service

--- bin/stadfangaskra.go
+++ bin/stadfangaskra.go
@@ -1,31 +1,54 @@
 package main
 
 import (
-	"code.google.com/p/gorilla/mux"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/StefanKjartansson/stadfangaskra"
-	"log"
+	log "github.com/llimllib/loglevel"
 	"net/http"
-)
-
-var (
-	router = new(mux.Router)
+	"os"
 )
 
 func main() {
-	var http_listen, placename_file string
-	flag.StringVar(&http_listen, "http", "127.0.0.1:3999", "host:port to listen on")
-	flag.StringVar(&placename_file, "file", "Stadfangaskra_20130326.dsv", "csv input file")
+
+	var httpListen, logLevel string
+
+	flag.StringVar(&httpListen, "http", "127.0.0.1:3999", "host:port to listen on")
+	flag.StringVar(&logLevel, "level", "info", "log level")
+
 	flag.Parse()
 
-	stadfangaskra.ImportDatabase(placename_file)
-	log.Println("Data Imported")
-	log.Println("Starting server")
+	if flag.NArg() == 0 {
+		log.Fatalf("Missing file argument")
+	}
 
-	stadfangaskra.SetupRoutes(router)
+	log.SetPriorityString(logLevel)
 
-	http.Handle("/", router)
+	file, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Fatal(http.ListenAndServe(http_listen, nil))
-	log.Println("Bye")
+	locs := []stadfangaskra.Location{}
+	decoder := json.NewDecoder(file)
+
+	err = decoder.Decode(&locs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for idx, l := range locs {
+		locs[idx].Name = fmt.Sprintf("%s, %d %s", l.Name, l.Postcode, l.Municipality)
+		b, err := json.Marshal(l)
+		if err != nil {
+			panic(err)
+		}
+		locs[idx].JSONCache = b
+	}
+
+	log.Infof("Starting server on: %s", httpListen)
+	ls := stadfangaskra.NewLocationService("/locations/", locs)
+	http.Handle("/", ls.GetRouter())
+	log.Fatal(http.ListenAndServe(httpListen, nil))
 }
