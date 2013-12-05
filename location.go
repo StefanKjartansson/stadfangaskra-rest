cc5906e9ef4d416382b8b705cde5ca51df073f0e
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-12-05 11:27:40 +0000

    do some geo stuff in the future

--- location.go
+++ location.go
@@ -1,5 +1,10 @@
 package stadfangaskra
 
+type Point struct {
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
+}
+
 type Location struct {
 	ID           int    `json:"id"`
 	Name         string `json:"name"`
@@ -9,9 +14,6 @@ type Location struct {
 	Street       string `json:"street,omitempty"`
 	Postcode     int    `json:"postcode"`
 	Municipality string `json:"municipality,omitempty"`
-	Coordinates  struct {
-		X float64 `json:"x"`
-		Y float64 `json:"y"`
-	} `json:"coordinates"`
-	JSONCache []byte `json:"-"`
+	Coordinates  Point  `json:"coordinates"`
+	JSONCache    []byte `json:"-"`
 }
